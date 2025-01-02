763687d8177feff54100f0ea036d1f30256d8767
senna <kjy915875@gmail.com>
2025-01-02 17:23:55 +0900

    feat: EnableTemporalSage 추가

---
+++ src/main/java/com/senna/TemporalSage/annotations/EnableTemporalSage.java
@@ -0,0 +1,17 @@
+package com.senna.TemporalSage.annotations;
+
+import com.senna.TemporalSage.configs.TemporalSageImportConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({
+    TemporalSageImportConfiguration.class
+})
+public @interface EnableTemporalSage {
+
+}
